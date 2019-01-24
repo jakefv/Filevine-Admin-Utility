a491dd958f5160729d424b8ff9465d9af2813183
Jake Mecham <46611895+jakefv@users.noreply.github.com>
2019-01-24 10:17:31 -0700

    Revert "Update contentScript.js"
    This reverts commit 6184e0733f737edcebbcf9703af3a71c07c0ff43.

--- FV Admin Utility/contentScript.js
+++ FV Admin Utility/contentScript.js
@@ -1,7 +1,7 @@
 var observer = new MutationObserver(onMutation);
 observer.observe(document, {
-  childList: true,
-  subtree: true,
+  childList: true, // report added/removed nodes
+  subtree: true, // observe any descendant elements
 });
 
 function gettingElementByText(searchText) {
@@ -19,7 +19,6 @@ function getCurrentOrg() {
     });
 }
 var buttons = document.getElementsByTagName('button');
-
 //Finds Org Chooser and selects the org indicated in popup.html
 function onMutation(mutations) {
   getCurrentOrg()
@@ -32,12 +31,14 @@ function onMutation(mutations) {
   }
 
   if (mutations.length == 1) {
-
+    // optimize the most frequent scenario: one element is added/removed
     var added = mutations[0].addedNodes[0];
     if (!added || (added.localName !== 'button' && !added.children.length)) {
-     return;
+      // so nothing was added or non-button with no child elements
+      return;
     }
-
+  }
+  // button is supposed to be used rarely so there'll be just a few elements
   for (var i = 0, len = buttons.length; i < len; i++) {
     var button = buttons[i];
     if (!button.textContent.match(/Choose an Org/)) {
